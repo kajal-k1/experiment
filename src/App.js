508113902b36061d83d374c3f6ae5340eeecf2c1
geeta <kajalkashyap5778@gmail.com>
2025-07-07 12:24:17 +0530

    added

---
+++ src/App.js
@@ -0,0 +1,11 @@
+import React from 'react';
+import Navbar from './components/Navbar'; // import your new component
+
+function App() {
+  return (
+    <div>
+      <Navbar/>
+    </div>
+  );
+}
+export default App; 
